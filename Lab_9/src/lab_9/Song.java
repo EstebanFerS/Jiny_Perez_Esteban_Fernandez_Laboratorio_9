09472a1e09be883ecc978bb664ed984b34627288
Jiny-perez <jinyperez55@gmail.com>
2025-06-20 14:12:48 -0600

    clase song

--- Lab_9/src/lab_9/Song.java
+++ Lab_9/src/lab_9/Song.java
@@ -4,10 +4,56 @@
  */
 package lab_9;
 
-/**
- *
- * @author marye
- */
+import javax.swing.ImageIcon;
+
 public class Song {
+
+    private int codigo;
+    private String nombre;
+    private double precio;
+    private int sumaEstrellas;
+    private int totalReviews;
+    private ImageIcon imagenDisco;
+
+    public Song(int codigo, String nombre, double precio, ImageIcon imagenDisco) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.imagenDisco = imagenDisco;
+        
+        sumaEstrellas=0;
+        totalReviews=0;
+    }
     
+    public int getCodigo(){
+        return codigo;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public double getPrecio(){
+        return precio;
+    }
+    
+    public ImageIcon getImagenDisco(){
+        return imagenDisco;
+    }
+    
+    
+    public void addStars(int stars) {
+        if (stars >= 0 && stars <= 5) {
+            sumaEstrellas += stars;
+            totalReviews++;
+        }
+    }
+
+    public double songRating() {
+        if (totalReviews == 0) {
+            return 0;
+        }
+        return (double) sumaEstrellas / totalReviews;
+    }
+
 }
